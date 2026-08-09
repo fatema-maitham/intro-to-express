aa72c33c0c30f8f4a05a402fa287242807d1a73e
fatema-maitham <fatemamaitham1@gmail.com>
2026-08-09 13:46:01 +0300

    done

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express');
 
 const app = express();
 
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-// Define a route for the root URL
 app.get('/', (req, res) => {
     res.send('<h1>Hello, World!</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About Us</h1>');
+});
+
+app.get('/hello', (req, res) => {
+    // Accessing query parameters from the request
+    const name = req.query.name;
+    const age = req.query.age;
+
+    // Using the query parameters to customize the response
+    res.send(`Hello there, ${name}! I hear you are ${age} years old!`);
+});
+
+app.get('/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}!`);
+});
+
+app.get('/greet/:name', (req, res) => {
+    res.send(`Hello, ${req.params.name}!`);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
